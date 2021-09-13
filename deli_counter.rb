6738769966140b7c4368784f0c4fcd68a31e7b05
Caitlin Raby <caitlinraby@gmail.com>
2021-09-13 14:11:45 -0500

    commit tree 4df4e643ebb609ad96827042a27e08cd768fe006

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+    if array.length == 0
+      puts "The line is currently empty."
+    else
+      array.each_with_index do |name, index| 
+        puts " #{index+1}. #{name} "
+      end
+    end
+end
